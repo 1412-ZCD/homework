64f8df179b51e99c0f8a90a627c7c35e765b2d9f
1412-ZCD <118173291+1412-ZCD@users.noreply.github.com>
2023-04-01 16:46:20 +0800

    Add files via upload

---
+++ P78_3_9.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P78_3_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList();
+            bool[] isprime = new bool[100];
+            int i, j;
+            for(int n=2;n<isprime.Length;n++)
+            {
+                isprime[n] = true;
+            }
+            for( i = 2; i <isprime.Length; i++)
+            {
+                if (isprime[i]) 
+                {
+                    for (j = 2 * i; j <isprime.Length; j += i)
+                    {
+                        isprime[j] = false;
+                    }
+                    if (isprime[i])
+                    {
+                        array.Add(i);
+                    }
+                }
+            }
+            foreach(object o in array) 
+            {
+                Console.Write(o+" ");
+            }
+        }
+        
+    }
+}
