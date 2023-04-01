386a65b4415d7942e91fdc051f0b7d71b486b05d
1412-ZCD <118173291+1412-ZCD@users.noreply.github.com>
2023-04-01 16:45:07 +0800

    Add files via upload

---
+++ P78_3_5.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P78_3_5
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入两个不相等的自然数：");
+            int n=int.Parse(Console.ReadLine());
+            int m=int.Parse(Console.ReadLine());
+            int i;
+            int top =n <m ? m : n;
+            int down = n < m ? n : m;
+            ArrayList arrayList = new ArrayList();
+            for ( down = n < m ? n : m; down <= top-1; down++)
+            {
+                for( i = 2;i<= down; i++)
+                {
+                    if(down%i == 0)
+                    {
+                        break;
+                    }
+                }
+                if (i == down)
+                {
+                    arrayList.Add(down);
+                }
+            }
+           
+            foreach(object o in arrayList)
+            {
+                Console.Write(o+" ");
+            }
+        }
+    }
+}
