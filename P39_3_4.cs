8ce4e13b29435668a4491ff5a1a5364ede092dca
1412-ZCD <118173291+1412-ZCD@users.noreply.github.com>
2023-04-01 16:24:57 +0800

    Add files via upload

---
+++ P39_3_4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            string a = Console.ReadLine();
+            Console.Write("b=");
+            string b = Console.ReadLine();
+            double x = double.Parse(a);
+            double y = double.Parse(b);
+            Console.WriteLine ("a*b="+(x*y));
+        }
+    }
+}
