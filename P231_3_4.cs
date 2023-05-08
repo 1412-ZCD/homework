80302de338a0e7fb35e1f0f0aa22704342d5e135
ZCD <118173291+1412-ZCD@users.noreply.github.com>
2023-05-08 11:37:51 +0800

    Add files via upload

---
+++ P231_3_4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P231_3_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x ;
+            for (x = 0; x <= 40; x++)
+            {
+                if (x * 2 + 4 * (40 - x) == 100)
+                {
+                    Console.WriteLine("鸡的个数：" + x);
+                    Console.WriteLine("兔的个数："+(40-x));
+                }
+            }
+
+        }
+    }
+}
