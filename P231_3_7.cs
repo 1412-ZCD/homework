bb6a2cf123ccfae763ab39b9ef9c894d5da2acc3
ZCD <118173291+1412-ZCD@users.noreply.github.com>
2023-05-08 17:45:12 +0800

    Add files via upload

---
+++ P231_3_7.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P231_3_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int n=1;n<1000;n++)
+            {
+                int s = divsum(n);
+                if(n<s && divsum(s)==n)
+                {
+                    Console.WriteLine(n + "," + s);
+                }
+            }
+        }
+
+        public static int divsum(int n)
+        {
+            int s = 0;
+            for(int i = 1; i < n; i++)
+            {
+                if (n % i == 0) s += i;
+            }
+            return s;
+        }
+    }
+}
