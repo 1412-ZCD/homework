3132737116ffce6b5636b2f7613a5068f799a150
ZCD <118173291+1412-ZCD@users.noreply.github.com>
2023-05-08 17:27:08 +0800

    Add files via upload

---
+++ P231_3_5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P231_3_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n;
+            for (m = 0; m <= 33; m++)
+            {
+                for (n = 0; n <= 20; n++)
+                {
+                    if (3 * m + 5 * n + (100 - m - n) / 3.0 == 100)
+                    {
+                        Console.WriteLine("公鸡个数：" + m);
+                        Console.WriteLine("母鸡个数：" + n);
+                        Console.WriteLine("小鸡个数：" + (100 - m - n));
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
